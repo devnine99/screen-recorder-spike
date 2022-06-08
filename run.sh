af463aecab445dfe08fb35b8c2981055c1b54aab
devnine99 <devnine99@gmail.com>
2022-06-08 13:07:42 +0900

    initial commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo docker run -it --rm \
+sudo docker run -it --rm --privileged \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /root/.Xauthority:/root/.Xauthority \
   -v `pwd`/blackbox:/opt/screen-recorder/blackbox \
