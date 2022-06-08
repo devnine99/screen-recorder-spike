f9965c1fdf85acff068abbd912f1c28566067dfa
devnine99 <devnine99@gmail.com>
2022-06-08 11:48:46 +0900

    initial commit

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v ${HOME}/.Xauthority:${HOME}/.Xauthority -e DISPLAY=unix${DISPLAY} --name screen-recorder screen-recorder python run.py
+sudo docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v ${HOME}/.Xauthority:/root/.Xauthority -e DISPLAY=unix${DISPLAY} --name screen-recorder screen-recorder python run.py
