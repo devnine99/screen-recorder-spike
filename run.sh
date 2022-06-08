2b06dc841edb9fe7042d667297c8d2a06e6d90ec
devnine99 <devnine99@gmail.com>
2022-06-08 12:09:49 +0900

    initial commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo docker run -it --rm \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /root/.Xauthority:/root/.Xauthority \
-  -v ./blackbox:/opt/blackbox \
+  -v blackbox:/opt/blackbox \
   -e DISPLAY=unix${DISPLAY} \
   --name screen-recorder \
   screen-recorder python run.py
