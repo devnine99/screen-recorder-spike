85a8a3ebfe1f87df48439f20ae571388c3d60b47
devnine99 <devnine99@gmail.com>
2022-06-08 12:43:47 +0900

    initial commit

--- bash.sh
+++
@@ -1,7 +0,0 @@
-sudo docker run -it --rm \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /root/.Xauthority:/root/.Xauthority \
-  -v `pwd`/blackbox:/opt/blackbox \
-  -e DISPLAY=unix${DISPLAY} \
-  --name screen-recorder \
-  screen-recorder bash
