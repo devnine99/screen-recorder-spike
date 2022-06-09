8d2f84af194d0d203cf41d070c071b55d0345621
devnine99 <devnine99@gmail.com>
2022-06-09 16:52:05 +0900

    initial commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "0 * * * * find /opt/screen-recorder/media/* -name '*.mp4' -cmin +1 rm {} +" | crontab -
+echo "0 * * * * find /opt/screen-recorder/media/* -type d -cmin +1 rm {} +" | crontab -
 #echo "0 * * * * find /opt/screen-recorder/media/* -name '*.mp4' -ctime +MEDIA_DURATION -exec rm {} +" | crontab -
 service cron start
 python run.py
